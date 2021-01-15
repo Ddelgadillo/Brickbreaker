45294d97bd31a4bd22c24242347da4ae4863f546
Diego <ddelgadillo11@gmail.com>
2021-01-15 16:15:53 -0600

    Added wall collision to the ball

--- Engine/Ball.h
+++ Engine/Ball.h
@@ -10,7 +10,10 @@ public:
 	Ball(Vec2& pos, Vec2& vel);
 	void Update(float delta);
 	void Draw(Graphics& gfx) const;
+	bool WallCollision(const RectF& walls);
+	RectF GetRect() const;
 private:
+	static constexpr float mRadius = 7.0f;
 	Vec2 mPos;
 	Vec2 mVel;
 };
